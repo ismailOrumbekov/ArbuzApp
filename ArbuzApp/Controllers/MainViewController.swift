94b604458ead5afc54e1858c5a75d331db38f84b
ismailOrumbekov <ismailorumbekovv@gmail.com>
2024-05-17 18:35:47 +0500

    first build

---
+++ ArbuzApp/Controllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArbuzApp
+//
+//  Created by Исмаил Орумбеков on 17.05.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+
+
+}
+
